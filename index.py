d84ad7ecb653ca20298c436d92b2b164784cec21
Javier <topnaita@gmail.com>
2019-11-03 16:56:55 -0400

    add html code

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
 from flask import Flask #we call flask
 
-app = Flask(__name__) #we initialize flask, then we get an odject called app,
+app = Flask(__name__) #__name__ is the file that pytho offer to start de app
+#we initialize flask, then we get an odject called app,
 # as from this odject we can create route, initialize a server.
 
-@app.route('/')# this is the main website "app is an object"
+@app.route('/')# @ is a decorator. this is the main website "app is an object and .route is a method"
 def home(): #what gonna happen when I call this function.
-    return 'hello world'
+    return 'home page'
 
 @app.route('/about')#I copied this from above in order to create another route.
 def about(): 
